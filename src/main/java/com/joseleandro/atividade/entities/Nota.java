1852a88fd758bf191b82e19694cff8744711935b
JLeandrodc <leandro.frot@hotmail.com>
2024-04-20 21:08:24 -0300

    API funcionando

---
+++ src/main/java/com/joseleandro/atividade/entities/Nota.java
@@ -0,0 +1,67 @@
+package com.joseleandro.atividade.entities;
+
+public class Nota {
+    private Long id;
+    private String materia;
+    private Double nota;
+
+    public Nota(){
+
+    }
+
+    public Nota(Long id, String materia, Double nota) {
+        this.id = id;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Nota other = (Nota) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+  
+}
