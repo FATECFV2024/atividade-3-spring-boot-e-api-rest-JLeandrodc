b757bbec8f453dce57f0fabb89ceaaa25b2fa5d6
JLeandrodc <leandro.frot@hotmail.com>
2024-05-13 20:37:00 -0300

    up

--- src/main/java/com/joseleandro/atividade/entities/Aluno.java
+++ src/main/java/com/joseleandro/atividade/entities/Aluno.java
@@ -2,10 +2,12 @@ package com.joseleandro.atividade.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Aluno implements Serializable {
     private static final long serialVersionUID = 1L;
 
--- src/main/java/com/joseleandro/atividade/entities/Nota.java
+++ src/main/java/com/joseleandro/atividade/entities/Nota.java
@@ -1,6 +1,18 @@
 package com.joseleandro.atividade.entities;
 
-public class Nota {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Nota implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String materia;
     private Double nota;
